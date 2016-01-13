3548b995f0993b775792fdebbf98731ec18a7db2
Cloudox <cloudox@foxmail.com>
2016-01-13 15:59:14 +0800

    完成圆形图片显示

--- RoundedDemo/ViewController.m
+++ RoundedDemo/ViewController.m
@@ -9,14 +9,24 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+//设备的宽高
+#define SCREENHEIGHT      [UIScreen mainScreen].bounds.size.height
+#define SCREENWIDTH       [UIScreen mainScreen].bounds.size.width
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    UIImageView *avatarImgView = [[UIImageView alloc] initWithFrame:CGRectMake((SCREENWIDTH - 75) / 2, 150, 75, 75)];
+    avatarImgView.image = [UIImage imageNamed:@"icon"];
+    // 图片做圆形，圆角角度为宽度的一般，而高宽一直，因此可以形成正圆
+    avatarImgView.layer.masksToBounds = YES;
+    avatarImgView.layer.cornerRadius = avatarImgView.frame.size.width / 2 ;
+    [self.view addSubview:avatarImgView];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
